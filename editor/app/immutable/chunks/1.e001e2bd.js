6f2665901ae19cf886b760a840b0d5727f203f47
Tom Armitage <tom@infovore.org>
2023-11-30 11:09:41 +0000

    Bump editor whilst we'are at it.

--- editor/app/immutable/chunks/1.e001e2bd.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.81f9fc42.js";export{t as component};
--- editor/app/immutable/chunks/2.efd91db4.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.3ec30ad7.js";export{t as component};
