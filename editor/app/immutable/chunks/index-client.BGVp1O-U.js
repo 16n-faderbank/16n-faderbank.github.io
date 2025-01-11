5433a7cf617a7b6a31503c369106bbb8a74945eb
Tom Armitage <tom@infovore.org>
2025-01-11 16:47:11 +0000

    Add editor 2.1.0

---
+++ editor/app/immutable/chunks/index-client.BGVp1O-U.js
@@ -0,0 +1,1 @@
+import{e as o,l as t,f as a,u as c,g as u}from"./runtime.pYtt92vo.js";function l(n){o===null&&t(),a&&o.l!==null?f(o).m.push(n):c(()=>{const e=u(n);if(typeof e=="function")return e})}function i(n){o===null&&t(),l(()=>()=>u(n))}function f(n){var e=n.l;return e.u??(e.u={a:[],b:[],m:[]})}export{i as a,l as o};
